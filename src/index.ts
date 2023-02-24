f624bf6848b276d5809a97e9032250836a0e4edd
aguing <aguing@outlook.com>
2023-02-24 16:38:48 +0800

    feat(global): add lint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
 export function myFunction(): void {
-    console.log('Hello, world!');
-  }
-  
+  console.log('Hello, world!')
+}
+// 将小程序/小游戏异步接口转为同步接口
+export default function promisify(fn: (params: unknown) => void) {
+  return (args = {}) =>
+    new Promise((resolve, reject) => {
+      fn(
+        Object.assign(args, {
+          success: resolve,
+          fail: reject,
+        })
+      )
+    })
+}
