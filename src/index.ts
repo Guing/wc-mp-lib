582ecc64cdb5f3899ac940d2b13a84ba221afee1
aguing <aguing@outlook.com>
2023-02-24 17:54:35 +0800

    feat(global): add jest

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-export function myFunction(): void {
-  console.log('Hello, world!')
-}
-// 将小程序/小游戏异步接口转为同步接口
-export default function promisify(fn: (params: unknown) => void) {
+/**
+ * 将小程序/小游戏异步接口转为同步接口
+ * @date 2023/2/24 - 17:13:52
+ *
+ * @export
+ * @param {(params: any) => void} fn 要转转换的函数
+ * @returns {void) => (args?: {}) => any} 无
+ */
+export function promisify(fn: (params: any) => void) {
   return (args = {}) =>
     new Promise((resolve, reject) => {
       fn(
